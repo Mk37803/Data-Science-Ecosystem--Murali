e470ea1c5d12287fed0ab2e7c1ce5b1b1baabaec
Mk37803 <132596992+Mk37803@users.noreply.github.com>
2023-05-04 21:44:47 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# #### In this notebook,Data science tools and Ecosystem are summarized 
+
+# ### __Objectives :__
+# - List Popular Languages that Data Scientists use.
+# - List of Data Science development Tools that are open source.
+# - Mathematical functions in Python 
+
+# #### Some of the popular languages that data scientists use are : 
+# 1. Python
+# 2. Java
+# 3. C
+# 4. C++ 
+
+# |Data science Tools |   
+# |---|
+# |Tensor Flow|
+# | Weka      |   
+# | Pandas    |
+# 
+
+# ### Below are a few examples of evaluating expressions in Python 
+
+# In[10]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[16]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+       
+
+
+# ## Author 
+# Murali Krishna
+
+# In[ ]:
+
+
+
+
